3ead5b02ec8b4679e5a1d7028081270ae01e51ff
David Sletten <david@bosatsu.net>
2020-08-15 21:36:14 -0400

    Working on SCRAWL.

--- touretzky-sdraw.lisp
+++ touretzky-sdraw.lisp
@@ -41,8 +41,6 @@
 ;;; bit maps rather than as character sequences.
 
 (defparameter *sdraw-display-width* 79) 
-;(defparameter *sdraw-horizontal-atom-cutoff* 79) 
-;(defparameter *sdraw-horizontal-cons-cutoff* 65)
 
 (defparameter *etc-string* "etc.") 
 (defparameter *circ-string* "circ.") 
@@ -55,7 +53,6 @@
 (defparameter *cons-v-arrow-offset-threshold* 2) 
 (defparameter *cons-v-arrow-offset-value* 1)
 
-(defparameter *sdraw-vertical-cutoff* 22) 
 (defparameter *sdraw-num-lines* 25 "The maximum depth allocated for the display of an object. Essentially three times the number of nested levels allowed.")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
@@ -64,34 +61,41 @@
 
 (defclass renderer ()
   ((display-width :reader display-width :initform *sdraw-display-width* :initarg :display-width)
-   (horizontal-atom-cutoff :reader horizontal-atom-cutoff :initform *sdraw-horizontal-atom-cutoff* :initarg :horizontal-atom-cutoff)
-   (horizontal-cons-cutoff :reader horizontal-cons-cutoff :initform *sdraw-horizontal-cons-cutoff* :initarg :horizontal-cons-cutoff)
+   (horizontal-atom-cutoff :reader horizontal-atom-cutoff :initarg :horizontal-atom-cutoff)
+   (horizontal-cons-cutoff :reader horizontal-cons-cutoff :initarg :horizontal-cons-cutoff)
    (num-lines :reader num-lines :initform *sdraw-num-lines* :initarg :num-lines)
+   (vertical-cutoff :reader vertical-cutoff)
    (line-endings :reader line-endings)))
 
 (defmethod initialize-instance :after ((r renderer) &rest initargs)
   (declare (ignore initargs))
-  (with-slots (num-lines line-endings) r
-    (setf line-endings (make-array num-lines))
-    (fill line-endings most-negative-fixnum)))
+  (with-slots (num-lines line-endings vertical-cutoff) r
+    (setf line-endings (make-array num-lines :initial-element most-negative-fixnum)
+          vertical-cutoff (- num-lines 3))))
+
+(defgeneric draw-structure (renderer directions))
 
 (defun sdraw (obj &key
                     (display-width *sdraw-display-width*)
                     (horizontal-atom-cutoff display-width)
                     (horizontal-cons-cutoff (- display-width 14))
                     (num-lines *sdraw-num-lines*))
-  (let ((renderer (make-instance 'tty :display-width display-width :horizontal-atom-cutoff horizontal-atom-cutoff :horizontal-cons-cutoff horizontal-cons-cutoff :num-lines num-lines)))
+  (let ((renderer (make-instance 'tty 
+                                 :display-width display-width
+                                 :horizontal-atom-cutoff horizontal-atom-cutoff
+                                 :horizontal-cons-cutoff horizontal-cons-cutoff
+                                 :num-lines num-lines)))
     (draw-structure renderer (struct1 renderer obj 0 0 nil)) 
     (values)))
 
-(defun struct1 (renderer obj row root-col obj-memory) 
+(defun struct1 (renderer obj row root-col cache) 
   (cond ((atom obj)
          (struct-process-atom renderer (format nil "~S" obj) row root-col)) 
-        ((member obj obj-memory :test #'eq)
+        ((member obj cache :test #'eq)
          (struct-process-circ renderer row root-col))
-        ((>= row *sdraw-vertical-cutoff*)
+        ((>= row (vertical-cutoff renderer))
          (struct-process-etc renderer row root-col))
-        (t (struct-process-cons renderer obj row root-col (cons obj obj-memory)))) )
+        (t (struct-process-cons renderer obj row root-col (cons obj cache)))) )
 
 (defun struct-process-atom (renderer atom-string row root-col) 
   (let* ((start-col (struct-find-start renderer row root-col))
@@ -101,19 +105,19 @@
            `(atom ,row ,start-col ,atom-string))
           (t (struct-process-etc renderer row root-col)))) )
 
-(defun struct-process-cons (renderer obj row root-col obj-memory)
+(defun struct-process-cons (renderer obj row root-col cache)
   (let* ((cons-start (struct-find-start renderer row root-col))
          (car-structure (struct1 renderer
                                  (car obj)
                                  (+ row *inter-cons-v-arrow-length*)
-                                 cons-start obj-memory))
+                                 cons-start cache))
          (start-col (third car-structure)))
     (if (>= start-col (horizontal-cons-cutoff renderer))
         (struct-process-etc renderer row root-col)
         `(cons ,row ,start-col ,car-structure
                ,(struct1 renderer (cdr obj) row
                          (+ start-col *cons-atom-h-arrow-length*) 
-                         obj-memory)))) )
+                         cache)))) )
 
 (defun struct-process-etc (renderer row root-col)
   "Row is too long. Ellide ending."
@@ -139,122 +143,51 @@
   (with-slots (line-endings) renderer
     (setf (aref line-endings row) end-col)))
 
-;; (defun sdraw (obj &key
-;;                     (display-width *sdraw-display-width*)
-;;                     (horizontal-atom-cutoff display-width)
-;;                     (horizontal-cons-cutoff (- display-width 14))
-;;                     (num-lines *sdraw-num-lines*))
-;;   (declare (special display-width horizontal-atom-cutoff horizontal-cons-cutoff num-lines))
-;;   (let ((line-endings (make-array num-lines)))
-;;     (declare (special line-endings))
-;;     (fill line-endings most-negative-fixnum) 
-;;     (draw-structure (struct1 obj 0 0 nil)) 
-;;     (values)))
-
-;; (defun struct1 (obj row root-col obj-memory) 
-;;   (cond ((atom obj)
-;;          (struct-process-atom (format nil "~S" obj) row root-col)) 
-;;         ((member obj obj-memory :test #'eq)
-;;          (struct-process-circ row root-col))
-;;         ((>= row *sdraw-vertical-cutoff*)
-;;          (struct-process-etc row root-col))
-;;         (t (struct-process-cons obj row root-col (cons obj obj-memory)))))
-
-;; (defun struct-process-atom (atom-string row root-col) 
-;;   (declare (special horizontal-atom-cutoff))
-;;   (let* ((start-col (struct-find-start row root-col))
-;;          (end-col (+ start-col (length atom-string)))) 
-;;     (cond ((< end-col horizontal-atom-cutoff)
-;;            (struct-record-position row end-col)
-;;            `(atom ,row ,start-col ,atom-string))
-;;           (t (struct-process-etc row root-col)))))
-
-;; (defun struct-process-cons (obj row root-col obj-memory)
-;;   (declare (special horizontal-cons-cutoff))
-;;   (let* ((cons-start (struct-find-start row root-col))
-;;          (car-structure (struct1 (car obj)
-;;                                  (+ row *inter-cons-v-arrow-length*)
-;;                                  cons-start obj-memory))
-;;          (start-col (third car-structure)))
-;;     (if (>= start-col horizontal-cons-cutoff)
-;;         (struct-process-etc row root-col)
-;;         `(cons ,row ,start-col ,car-structure
-;;                ,(struct1 (cdr obj) row
-;;                          (+ start-col *cons-atom-h-arrow-length*) 
-;;                          obj-memory)))))
-
-;; (defun struct-process-etc (row root-col)
-;;   "Row is too long. Ellide ending."
-;;   (let ((start-col (struct-find-start row root-col)))
-;;     (struct-record-position 
-;;      row
-;;      (+ start-col (length *etc-string*) *etc-spacing*))
-;;     `(msg ,row ,start-col ,*etc-string*)))
-
-;; (defun struct-process-circ (row root-col)
-;;   "Circular structure detected."
-;;   (let ((start-col (struct-find-start row root-col)))
-;;     (struct-record-position 
-;;      row
-;;      (+ start-col (length *circ-string*) *circ-spacing*))
-;;     `(msg ,row ,start-col ,*circ-string*)))
- 
-;; (defun struct-find-start (row root-col)
-;;   (declare (special line-endings))
-;;   (max root-col (+ *inter-atom-h-spacing* (aref line-endings row))))
-
-;; (defun struct-record-position (row end-col) 
-;;   (declare (special line-endings))
-;;   (setf (aref line-endings row) end-col))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
 ;;;
 ;;; SDRAW-LOOP and subordinate definitions.
 
 (defparameter *sdraw-loop-prompt-string* "S> ")
 
-(defun sdraw-loop ()
+(defun sdraw-loop (&key (display-width *sdraw-display-width*))
   "Read-eval-print loop using sdraw to display results."
+  (declare (special display-width))
   (format t "~&Type any Lisp expression, or (ABORT) to exit.~%~%") 
   (sdl1))
 
 (defun sdl1 ()
   (loop
      (format t "~&~A" *sdraw-loop-prompt-string*)
+     (force-output)
      (let ((form (read)))
-       (setf +++ ++ 
-             ++ + 
-             + -
-             - form)
+       (shiftf +++ ++ + - form)
        (let ((result (multiple-value-list
                       (handler-case (eval form)
                         (error (condx) condx)))))
          (typecase (first result)
            (error (display-sdl-error result))
-           (t (setf /// //
-                    // /
-                    / result
-                    *** **
-                    ** *
-                    * (first result))
+           (t (shiftf /// // / result)
+              (shiftf *** ** * (first result))
               (display-sdl-result *)))))))
 
 (defun display-sdl-result (result) 
+  (declare (special display-width))
   (let* ((*print-circle* t)
          (*print-length* nil)
          (*print-level* nil)
          (*print-pretty* nil)
          (full-text (format nil "Result: ~S" result))
-         (text (if (> (length full-text)
-                      *sdraw-display-width*)
-                   (concatenate 'string
-                                 (subseq full-text 0 (- *sdraw-display-width* 4))
-                                 "...)") 
+         (text (if (> (length full-text) display-width)
+                   (ellide full-text display-width)
                    full-text)))
-    (sdraw result)
-    (if (consp result)
-        (format t "~%~A~%" text))
+    (sdraw result :display-width display-width)
+    (when (consp result)
+      (format t "~%~A~%" text))
     (terpri)))
+
+(defun ellide (s width)
+  (let ((ellipsis "...)"))
+    (concatenate 'string (subseq s 0 (- width (length ellipsis))) ellipsis)))
 
 (defun display-sdl-error (error) 
   (format t "~A~%~%" error))
@@ -264,89 +197,94 @@
 ;;; SCRAWL and subordinate definitions.
 
 (defparameter *scrawl-prompt-string* "SCRAWL> ") 
-(defvar *scrawl-object* nil)
-(defvar *scrawl-current-obj*)
-(defvar *extracting-sequence* nil)
+;; (defvar *scrawl-object* nil)
+;; (defvar *scrawl-current-obj*)
+;; (defvar *extracting-sequence* nil)
 
-(defun scrawl (obj)
+(defun scrawl (obj &key (display-width *sdraw-display-width*))
   "Read-eval-print loop to travel through list"
+  (declare (special display-width))
   (format t "~&Crawl through list: 'H' for help, 'Q' to quit.~%~%") 
-  (setf *scrawl-object* obj)
-  (setf *scrawl-current-obj* obj)
-  (setf *extracting-sequence* nil)
-  (sdraw obj)
-  (scrawl1))
+  (let ((scrawl-object obj)
+        (scrawl-current-obj obj)
+        (extracting-sequence nil))
+    (declare (special scrawl-object scrawl-current-obj extracting-sequence))
+    (sdraw obj :display-width display-width)
+    (scrawl1)))
 
 (defun scrawl1 ()
   (loop
      (format t "~&~A" *scrawl-prompt-string*) 
+     (force-output)
      (let ((command (read-uppercase-char)))
        (case command
          (#\A (scrawl-car-cmd))
          (#\D (scrawl-cdr-cmd))
-         (#\B (scrawl-back-up-cmd)) (#\S (scrawl-start-cmd))
-         (#\H (display-scrawl-help)) (#\Q (return))
+         (#\B (scrawl-back-up-cmd)) 
+         (#\S (scrawl-start-cmd))
+         (#\H (display-scrawl-help)) 
+         (#\Q (return))
          (t (display-scrawl-error))))))
 
 (defun scrawl-car-cmd ()
-  (cond ((consp *scrawl-current-obj*)
-         (push 'car *extracting-sequence*)
-         (setf *scrawl-current-obj* (car *scrawl-current-obj*))) 
+  (declare (special scrawl-current-obj extracting-sequence))
+  (cond ((consp scrawl-current-obj)
+         (push 'car extracting-sequence)
+         (setf scrawl-current-obj (car scrawl-current-obj))) 
         (t (format t
-                   "~&Can't take CAR or CDR of an atom. Use B to back up.~%"))) 
+                   "~&Can't take CAR or CDR of an atom. Use B to back up.~%")))
   (display-scrawl-result))
  
 (defun scrawl-cdr-cmd ()
-  (cond ((consp *scrawl-current-obj*)
-         (push 'cdr *extracting-sequence*)
-         (setf *scrawl-current-obj* (cdr *scrawl-current-obj*))) 
+  (declare (special scrawl-current-obj extracting-sequence))
+  (cond ((consp scrawl-current-obj)
+         (push 'cdr extracting-sequence)
+         (pop scrawl-current-obj))
+;         (setf *scrawl-current-obj* (cdr *scrawl-current-obj*)))  ;; Symmetry above???
         (t (format t
                    "~&Can't take CAR or CDR of an atom. Use B to back up.~%"))) 
   (display-scrawl-result))
 
 (defun scrawl-back-up-cmd () 
-  (cond (*extracting-sequence*
-         (pop *extracting-sequence*) 
-         (setf *scrawl-current-obj*
-               (extract-obj *extracting-sequence* *scrawl-object*))) 
-        (t (format t "~&Already at beginning of object.")))
+  (declare (special scrawl-object scrawl-current-obj extracting-sequence))
+  (cond ((null extracting-sequence) (format t "~&Already at beginning of object."))
+        (t (pop extracting-sequence) 
+           (setf scrawl-current-obj
+                 (extract-obj extracting-sequence scrawl-object))))
   (display-scrawl-result))
 
 (defun scrawl-start-cmd ()
-  (setf *scrawl-current-obj* *scrawl-object*)
-  (setf *extracting-sequence* nil)
+  (declare (special scrawl-object scrawl-current-obj extracting-sequence))
+  (setf scrawl-current-obj scrawl-object
+        extracting-sequence nil)
   (display-scrawl-result))
 
 (defun extract-obj (seq obj) 
-  (reduce #'funcall
-            seq 
-            :initial-value obj
-            :from-end t))
+  (reduce #'funcall seq :initial-value obj :from-end t))
 
 (defun get-car/cdr-string ()
-  (if (null *extracting-sequence*)
-      (format nil "'~S" *scrawl-object*)
+  (declare (special scrawl-object extracting-sequence))
+  (if (null extracting-sequence)
+      (format nil "'~S" scrawl-object)
       (format nil "(c~Ar '~S)"
               (map 'string #'(lambda (x)
                                (ecase x
                                  (car #\a)
                                  (cdr #\d))) 
-                    *extracting-sequence*)
-              *scrawl-object*)))
+                    extracting-sequence)
+              scrawl-object)))
 
 (defun display-scrawl-result ()
+  (declare (special display-width scrawl-current-obj))
   (let* ((*print-pretty* nil)
          (*print-length* nil)
          (*print-level* nil)
          (*print-circle* t)
          (extract-string (get-car/cdr-string))
-         (text (if (> (length extract-string) *sdraw-display-width*) 
-                   (concatenate 'string
-                                (subseq extract-string 0
-                                        (- *sdraw-display-width* 4))
-                                "...)") 
+         (text (if (> (length extract-string) display-width) 
+                   (ellide extract-string display-width)
                    extract-string)))
-    (sdraw *scrawl-current-obj*)
+    (sdraw scrawl-current-obj :display-width display-width)
     (format t "~&~%~A~%~%" text)))
 
 (defun display-scrawl-help ()
@@ -381,13 +319,11 @@
   (declare (ignore initargs))
   (with-slots (display-width num-lines textline-array textline-lengths) tty
     (setf textline-array (make-array num-lines)
-          textline-lengths (make-array num-lines))
+          textline-lengths (make-array num-lines :initial-element 0))
     (dotimes (i (length textline-array))
       (setf (aref textline-array i) 
-            (make-string display-width)))
-    (fill textline-lengths 0)))
+            (make-string display-width)))) )
 
-(defgeneric draw-structure ((r renderer) directions))
 (defmethod draw-structure ((tty tty) directions) 
   (follow-directions tty directions) 
   (dump-display tty))
@@ -418,12 +354,12 @@
 (defun draw-msg (tty directions is-car)
   (destructuring-bind (type row col string) directions
     (declare (ignore type))
-  (char-blt tty 
-            row
-            (+ col (if (and is-car (<= (length string) *cons-v-arrow-offset-threshold*)) 
-                       *cons-v-arrow-offset-value*
-                       0))
-            string)))
+    (char-blt tty 
+              row
+              (+ col (if (and is-car (<= (length string) *cons-v-arrow-offset-threshold*)) 
+                         *cons-v-arrow-offset-value*
+                         0))
+              string)))
 
 (defun char-blt (tty row start-col string)
   "Clear out the existing text in ROW up to position START-COL."
@@ -445,67 +381,3 @@
           while (plusp len)
           do (format t "~&~A" (subseq line 0 len)))
     (terpri)))
-;; (defun draw-structure (directions) 
-;;   (declare (special display-width num-lines))
-;;   (let ((textline-array (make-array num-lines))
-;;         (textline-lengths (make-array num-lines)))
-;;     (declare (special textline-array textline-lengths))
-;;     (dotimes (i (length textline-array))
-;;       (setf (aref textline-array i) 
-;;             (make-string display-width)))
-;;     (fill textline-lengths 0) 
-;;     (follow-directions directions) 
-;;     (dump-display)))
-
-;; (defun follow-directions (dirs &optional is-car) 
-;;   (ecase (car dirs)
-;;     (cons (draw-cons dirs))
-;;     ((atom msg) (draw-msg dirs is-car))))
-
-;; (defun char-blt (row start-col string)
-;;   "Clear out the existing text in ROW up to position START-COL."
-;;   (declare (special textline-array textline-lengths))
-;;   (let ((spos (aref textline-lengths row))
-;;         (line (aref textline-array row))) 
-;;     (fill line #\Space :start spos :end start-col)
-;;     (replace line string :start1 start-col)
-;;     (setf (aref textline-lengths row)
-;;           (+ start-col (length string)))))
-
-;; (defun draw-cons (directions)
-;;   (declare (special textline-array textline-lengths))
-;;   (destructuring-bind (type row col car-component cdr-component) directions
-;;     (declare (ignore type))
-;;     (let ((line (aref textline-array row))
-;;           (h-arrow-start (+ col *cons-cell-flatsize*))
-;;           (h-arrowhead-col (1- (third cdr-component))))
-;;       (char-blt row col *cons-string*) 
-;;       (do ((i h-arrow-start (1+ i)))
-;;           ((>= i h-arrowhead-col))
-;;         (setf (aref line i) *cons-h-arrowshaft-char*))
-;;       (setf (aref line h-arrowhead-col) *cons-h-arrowhead-char*)
-;;       (setf (aref textline-lengths row) (1+ h-arrowhead-col)) 
-;;       (char-blt (+ row 1) (+ col 1) *cons-v-line*)
-;;       (char-blt (+ row 2) (+ col 1) *cons-v-arrowhead*)
-;;       (follow-directions car-component t)
-;;       (follow-directions cdr-component))))
-
-;; (defun draw-msg (directions is-car)
-;;   (destructuring-bind (type row col string) directions
-;;     (declare (ignore type))
-;;   (char-blt row
-;;             (+ col (if (and is-car (<= (length string) *cons-v-arrow-offset-threshold*)) 
-;;                        *cons-v-arrow-offset-value*
-;;                        0))
-;;             string)))
-
-;; (defun dump-display ()
-;;   "Actually print the structure to the screen."
-;;   (declare (special textline-array textline-lengths))
-;;   (terpri)
-;;   (loop for i from 0 below (length textline-array)
-;;         for len = (aref textline-lengths i)
-;;         for line = (aref textline-array i)
-;;         while (plusp len)
-;;         do (format t "~&~A" (subseq line 0 len)))
-;;   (terpri))
