1547d8dc9b2eec84c2b2c9bd640d55905e2943c6
David Sletten <david@bosatsu.net>
2020-12-09 02:10:24 -0500

    Sweet clean up. Done with chapter 13.

--- 2020/cryptogram.lisp
+++ 2020/cryptogram.lisp
@@ -39,13 +39,13 @@
 (defvar *encipher-table* (make-hash-table :test #'equalp))
 (defvar *decipher-table* (make-hash-table :test #'equalp))
 
-(defun make-substitution (in out)
-  (setf (gethash in *encipher-table*) out)
-  (setf (gethash out *decipher-table*) in))
+(defun make-substitution (clr enc)
+  (setf (gethash clr *encipher-table*) enc
+        (gethash enc *decipher-table*) clr))
 
-(defun undo-substitution (ch)
-  (remhash (decode-char ch) *encipher-table*)
-  (remhash ch *decipher-table*))
+(defun undo-substitution (clr enc)
+  (remhash clr *encipher-table*)
+  (remhash enc *decipher-table*))
 
 (defun clear ()
   (clrhash *encipher-table*)
@@ -53,46 +53,65 @@
 
 (defun decipher-string (encoded)
 ;  (let ((decoded (make-string (length encoded) :initial-element #\space)))
-  (map 'string #'(lambda (ch) (gethash ch *decipher-table* #\space)) encoded))
+  (map 'string 
+       #'(lambda (enc) (if (alpha-char-p enc) (match-case enc (decode-char enc #\space)) enc)) 
+       encoded))
+
+(defun match-case (enc clr)
+  (cond ((not (alpha-char-p clr)) clr)
+        ((upper-case-p enc) (char-upcase clr))
+        (t clr)))
 
 (defun show-line (line)
   (format t "~A~%" line)
-  (format t "~A~%" (decipher-string line)))
+  (format t "~A~2%" (decipher-string line)))
 
 (defun show-text (text)
   (dolist (line text)
     (show-line line)))
 
-(defun decode-char (ch)
-  (gethash ch *decipher-table*))
+(defun decode-char (enc &optional default)
+  (gethash enc *decipher-table* default))
 
-(defun encode-char (ch)
-  (gethash ch *encipher-table*))
+(defun encode-char (clr)
+  (gethash clr *encipher-table*))
 
-(defun sub-letter (ch)
-  (let ((ch1 (decode-char ch)))
-    (if ch1
-        (warn "'~C' has alreaday been deciphered as '~C'!" ch ch1)
-        (let* ((ch1 (char-downcase (char (prompt-read (format nil "What does '~C' decipher to? " ch) :allow-empty nil) 0)))
-               (ch0 (encode-char ch1)))
-          (if ch0
-              (warn "But '~C' already deciphers to '~C'!" ch0 ch1)
-              (make-substitution ch1 ch)))) ))
+(defun sub-letter (enc)
+  (let ((clr (decode-char enc)))
+    (if clr
+        (warn "'~C' has alreaday been deciphered as '~C'!" enc clr)
+        (let* ((clr (char-downcase (char (prompt-read (format nil "What does '~C' decipher to? " enc) :allow-empty nil) 0)))
+               (enc* (encode-char clr)))
+          (if enc*
+              (warn "But '~C' already deciphers to '~C'!" enc* clr)
+              (make-substitution clr enc)))) ))
 
 (defun undo-letter ()
-  (let* ((ch (char-downcase (char (prompt-read "Undo which letter?" :allow-empty nil) 0)))
-         (ch1 (decode-char ch)))
-    (if ch1
-        (undo-substitution ch)
-        (warn "'~C' has not been deciphered yet." ch))))
+  (let* ((enc (char-downcase (char (prompt-read "Undo which letter? " :allow-empty nil) 0))) ; Check input!
+         (clr (decode-char enc)))
+    (if clr
+        (undo-substitution clr enc)
+        (warn "'~C' has not been deciphered yet." enc))))
+
+;; (defun solvedp (cryptogram)
+;;   (etypecase cryptogram
+;;     (list (every #'solvedp cryptogram))
+;;     (string (loop for ch across cryptogram
+;;                   when (alpha-char-p ch)
+;;                     unless (decode-char ch)
+;;                       return nil
+;;                     end
+;;                   end
+;;                   finally (return t)))) )
 
 (defun solvedp (cryptogram)
   (etypecase cryptogram
+    (character (if (alpha-char-p cryptogram) (decode-char cryptogram) t))
     (list (every #'solvedp cryptogram))
-    (string (dotimes (i (length cryptogram) t)
-              (unless (or (char= (char cryptogram i) #\space)
-                          (decode-char (char cryptogram i)))
-                (return nil)))) ))
+    (string (loop for ch across cryptogram
+                  unless (solvedp ch)
+                    return nil
+                  finally (return t)))) )
 
 (defun solve (cryptogram)
   (labels ((execute ()
@@ -106,4 +125,10 @@
                       (execute)))) )
     (execute)))
 
-
+;;;
+;;;    See 2002 version for more examples.
+;;;    
+;"M cef tj e negb pbzbjgih, ejn gub vqh gttr xb utpfbsezr" "pmnmjv. gueg cef rmjn tw wqj, qjgmi cb pej tqg tw" "kqepgbpf." "--Fqfmb Itqzrf"
+; i was on a date recently, and the guy took me horseback
+; riding. that was kind of fun, until we ran out of
+; quarters.--susie loucks
